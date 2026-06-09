312bcb1f8bc6a2f690536cebd197077b96976ea0
MahanSinghRajput <mahansingh01@icloud.com>
2026-06-09 11:12:58 +0000

    LL implementation

---
+++ LinkedList/Implementation.java
@@ -0,0 +1,56 @@
+public class Implementation {
+    static class Node{
+        int val;
+        Node next;
+        Node(int val){
+            this.val = val;
+        }
+    }
+
+    static class linkedlist{
+        Node head = null;
+        Node tail = null;
+        
+        void insertAtEnd(int data){
+            Node temp = new Node(data);
+
+            if(head == null){
+                head = temp;
+                tail = temp;
+            }else{
+                tail.next = temp;
+                tail = temp;
+            }
+        }
+
+        void insertAtStart(int data){
+            Node temp = new Node(data);
+
+            if(head == null){
+                head = temp;
+                tail = temp;
+            }else{
+                temp.next = head;
+                head = temp;
+            }
+        }
+
+        void insert(int data, int index){
+            Node newNode = new Node(data);
+            Node temp = head;
+            if(index == 0) insertAtStart(data);
+            // if(index == size()) insertAtEnd(data);
+            for(int i=0;i<index-1;i++){
+                temp = temp.next;
+            }
+            newNode.next = temp.next;
+            temp.next = newNode;
+        }
+
+    }
+    public static void main(String[] args){
+        linkedlist ll = new linkedlist();
+        ll.insertAtEnd(2);
+        ll.insertAtEnd(3);
+    }
+}
